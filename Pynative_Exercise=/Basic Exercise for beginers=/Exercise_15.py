e890c9c45d827f43141de25133ec90bd889abd6f
Pruthviraj Chavan <pruthvirajchavan0104@gmail.com>
2026-07-25 20:10:12 +0530

    Add files via upload

---
+++ Pynative_Exercise=/Basic Exercise for beginers=/Exercise_15.py
@@ -0,0 +1,11 @@
+# Nested Loops for Pattern Generation
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i,end=' ')
+    print('\n')
+# for num in range(1, 6):
+#     # Inner loop for repetition
+#     for i in range(num):
+#         print(num, end=" ") # end=" " keeps it on the same line
+#     # New line after each row
+#     print("\n")
