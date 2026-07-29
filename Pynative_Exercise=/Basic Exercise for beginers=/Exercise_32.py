458770d7b24be296f91385a76f584003f25bad96
Pruthviraj Chavan <pruthvirajchavan0104@gmail.com>
2026-07-29 21:56:17 +0530

    Add files via upload

---
+++ Pynative_Exercise=/Basic Exercise for beginers=/Exercise_32.py
@@ -0,0 +1,7 @@
+'''Practice Problem: 
+Create a dictionary where the keys are numbers from 1 to 10 and
+ the values are the squares of those numbers (e.g., 2: 4, 3: 9).'''
+dictionary={}
+for i in range(1,11):
+    dictionary[i]=i*i
+print(dictionary)
