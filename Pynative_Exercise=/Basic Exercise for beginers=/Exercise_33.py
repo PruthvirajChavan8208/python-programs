d9b0a962bac6300ee1ff54b8c61ba532acb8613d
Pruthviraj Chavan <pruthvirajchavan0104@gmail.com>
2026-07-29 21:56:43 +0530

    Add files via upload

---
+++ Pynative_Exercise=/Basic Exercise for beginers=/Exercise_33.py
@@ -0,0 +1,5 @@
+'''Practice Problem: 
+Ask the user for a sentence. 
+Replace every empty space in that sentence with an underscore (_) and print the final result.'''
+text = "I love coding in Python"
+print(text.replace(' ','_'))
