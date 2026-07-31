296168b6377d39ae9cb346fdb043c0b217db317d
PruthvirajChavan8208 <pruthvirajchavan0104@gmail.com>
2026-07-31 22:45:09 +0530

    Create Exercise11.py

---
+++ Pynative_Exercise=/Python Loop Exercise/Exercise11.py
@@ -0,0 +1,7 @@
+#Practice Problem: Write a program that counts the total number of vowels 
+#and consonants in a given sentence, ignoring spaces and special characters.
+org_str="python"
+reverse=""
+for i in org_str:
+    reverse = i+reverse
+print(reverse)
