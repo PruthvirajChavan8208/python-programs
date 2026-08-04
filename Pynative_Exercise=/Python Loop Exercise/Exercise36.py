d13531561e8067d6e85270c503d5d36e14cce1c8
PruthvirajChavan8208 <pruthvirajchavan0104@gmail.com>
2026-08-04 22:20:34 +0530

    Update Exercise36.py

--- Pynative_Exercise=/Python Loop Exercise/Exercise36.py
+++ Pynative_Exercise=/Python Loop Exercise/Exercise36.py
@@ -1,13 +1,10 @@
 '''Practice Problem: Manually convert a binary string (e.g., "1101") into its decimal integer equivalent using a loop. 
 Do not use int(binary, 2).'''
 binary_str = "1101"
-binary_str = "1101"
-decimal_val = 0
-
-reversed_binary = binary_str[::-1]
-
-for i in range(len(reversed_binary)):
-    if reversed_binary[i] == '1':
-        decimal_val += 2 ** i
-
-print("Decimal value:", decimal_val)
+sum=0
+reversed_str=binary_str[::-1]
+for i in range(len(reversed_str)):
+    if reversed_str[i] == '1':
+       
+        sum+=2 ** i
+print(sum)
