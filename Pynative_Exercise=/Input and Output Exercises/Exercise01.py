c310576f7b69b4dd38b015870280e91f58fbd785
PruthvirajChavan8208 <pruthvirajchavan0104@gmail.com>
2026-08-09 22:00:56 +0530

    practice-programs

---
+++ Pynative_Exercise=/Input and Output Exercises/Exercise01.py
@@ -0,0 +1,7 @@
+# Exercise 1. Accept numbers from a user
+# Practice Problem: Write a program that accepts two integer numbers from the user and 
+# calculates their multiplication. Print the final result to the console.
+a=int(input('Enter the first no:'))
+b=int(input('Enter the second no:'))
+
+print(f'Multiplication of {a} and {b} is:{a*b}')
---
+++ Pynative_Exercise=/Input and Output Exercises/Exercise02.py
@@ -0,0 +1,3 @@
+# Practice Problem: Use the print() function to format the words “Name”, “Is”, and 
+# “James” so they appear with three asterisks (***) between them.
+print('Name','is','james',sep='***')
