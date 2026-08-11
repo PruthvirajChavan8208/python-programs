f4cd9b30b5f0b9f40cbb304b62a54d09232d70e8
Pruthviraj Chavan <pruthvirajchavan0104@gmail.com>
2026-08-11 18:59:40 +0530

    Add files via upload
    Practice program

---
+++ Pynative_Exercise=/Input and Output Exercises/Exercise04.py
@@ -0,0 +1,4 @@
+#Practice Problem: Accept an integer from the user and display its 
+#value in binary format (Base 2).
+num=int(input("enter an integer number:"))
+print(f"{num} in binary format is{num:b}")
