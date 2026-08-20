0e860e53916f527ca5a6c5276702b9be752b7898
PruthvirajChavan8208 <pruthvirajchavan0104@gmail.com>
2026-08-20 21:53:43 +0530

    practice programs

---
+++ Pynative_Exercise=/Python Data Structure/Exercis02.py
@@ -0,0 +1,14 @@
+#Practice Problem: Write a script to perform the following three operations on given list
+
+#Access the third element of a list
+#List Length: Print the total number of items
+#Check if the list is empty
+
+list =[1,2,3,4,5]
+print(f"third element is {list[2]}")
+print (f"total length of the list is {len(list)}")
+if len(list) == 0:
+    print("empty")
+else:
+    print ("not empty")
+    
---
+++ Pynative_Exercise=/Python Data Structure/Exercise01.py
@@ -0,0 +1,5 @@
+#Practice Problem: Write a script to perform the following three operations on given list
+
+Access the third element of a list
+List Length: Print the total number of items
+Check if the list is empty
